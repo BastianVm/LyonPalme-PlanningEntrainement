4e6f1c94ce3da0f5269859fef2e2bc719cd8a541
Bastian VIVIER-MERLE <bastianviviermerle@leschassagnes.net>
2025-04-09 16:35:07 +0200

    Chiffrement Fonctionnel

--- LPPE/database/factories/LPPE_EntraineursFactory.php
+++ LPPE/database/factories/LPPE_EntraineursFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Crypt;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\LPPE_Entraineurs>
