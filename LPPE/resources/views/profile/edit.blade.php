a54d22ee87d1cd6719c3259e2a22b25c7c06304f
Bastian VIVIER-MERLE <bastianviviermerle@leschassagnes.net>
2025-06-05 13:09:13 +0200

    ajout fonctionnalité changer email et mdp, vue profile

--- LPPE/resources/views/profile/edit.blade.php
+++ LPPE/resources/views/profile/edit.blade.php
@@ -1,29 +1,17 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+                @include('profile.partials.update-profile-information-form', ['user' => $user])
             </div>
-
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+                @include('profile.partials.update-password-form', ['user' => $user])
             </div>
-
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+                @include('profile.partials.delete-user-form', ['user' => $user])
             </div>
         </div>
     </div>
-</x-app-layout>
+@endsection
